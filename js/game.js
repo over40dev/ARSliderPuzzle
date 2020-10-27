33c5e2e218149e71a146e6c758c0d0031e953c7e
over40dev <ted@over40.dev>
2020-10-26 23:14:35 -0230

    Part 1- getUserMedia() and takePhoto()

---
+++ js/game.js
@@ -0,0 +1,44 @@
+{
+  const
+    // image is new set to a new Image Object
+    image = new Image(),
+    // Get a reference to the HTML Take Photo Button by selecting the first DOM
+    // element with a CLASS of 'takePhoto'. The  ` . ` before the Class Name tells
+    // the selector that the name that follows the  ` . `  is a Class.
+    takePhotoButton = document.querySelector('.takePhoto');
+
+  let contraints,
+    imageCapture,
+    mediaStream,
+    video;
+  /**
+ *Get a list of the available media input and output devices, such as microphones, cameras, headsets, and so forth, using the  ` MediaDevices.enumerateDevices() `  function.  This function returns a **JS Promise**. We use the **catch** in case an error causes the operation to **rejects**.  We use a **then** to process the array of media devices if is successfully return.
+ */
+  const init = () => {
+    // From our HTML file, pull the AR.js video which is created within the A-Frame
+    // Scene using the embedded arjs properties
+    video = document.querySelector('video');
+
+    navigator
+      .mediaDevices
+      .enmerateDevices()
+      .catch(error => console.log('enmerateDevices() error', error))
+      .then(getStream);
+
+    takePhotoButton.addEventListener('click', getPicture);
+  }
+
+  // Get a video stream from teh camera
+  const getStream = () => {
+
+  }
+
+  const gotStream = (stream) => {
+
+  }
+
+  const getPicture = () => {
+    
+  }
+
+}
