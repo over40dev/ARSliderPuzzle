4355abbbcd6ef2974fadcd18bbc3a60e176f52d1
over40dev <ted@over40.dev>
2020-10-30 22:04:24 -0230

    09-add getStream() function

--- js/game.js
+++ js/game.js
@@ -1,26 +1,47 @@
 {
-  // f08
-
-  /* Create Game-Level Variables */
-
-  // get reference to DOM HTML Button with Class of 'takePhoto'
+  /* Existing Game-Level Variables */
   const takePhotoButton = document.querySelector('.takePhoto');
+  let video;
 
-  /* Create 'init' Function | runs when page loads */
+  // 09 | Add Game-Level Variables
+  let mediaStream;
 
   const init = () => {
-    // get access to 'video element' which is added by 'AR.js' when it processes page.
     video = document.querySelector('video');
-    // returns an array of media devices including camera available to us
-    // if successful, call 'getStream' function
-    // if fails, calls error handler
+
+    // new code
+    /* returns an array of media devices including camera available to us. If successful, call 'getStream' function if fails, calls error handler */
     navigator.mediaDevices.enumerateDevices()
       .then(getStream)
       .error((error) => console.log('enumerateDevices() error: ', error));
 
-    /* take picture when user clicks 'takePhotoButton' by listening for 'click' event and running a function*/
+    /* take picture when user presses 'takePhotoButton' by listening for 'click' event and running a function */
     takePhotoButton.addEventListener('click', getPicture);
   }
 
-  /* The 'init' function calls two other functions 'getStream' and 'getPicture' that we will code in the next two lessons */
+  // 09 | new code
+  
+  const getStream = () => {
+    
+    if (mediaStream) {
+      mediaStream.getTracks().forEach((track) => track.stop());
+    }
+    
+    const constraints = {
+      video: {
+        height: 720,
+        width: 720,
+      }
+    };
+    
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(gotStream)
+    .error((error) => console.log('getUserMedia() error: ', error));
+  };
+  
+  // 09 | new code
+
+  const gotStream = (stream) => {
+    
+  };
 }
