d388e627fce75b51dcb26963befe65cc4201c313
over40dev <ted@over40.dev>
2020-10-31 21:47:36 -0230

    f12 - Create Image Pieces - Part 2

--- js/game.js
+++ js/game.js
@@ -7,17 +7,16 @@
   let imageCapture;
   // f10
   const image = new Image();
-  // f11 - New Variables
+  // f11
   let numCol = 3, numRow = 3;
-  // f12
-  // const puzzlePieces = numCol * numRow;
-  // const imagePieces = new Array(puzzlePieces);
-  // const puzzle = [...imagePieces.keys()].map(String);
-  // let pieces = numCol * numRow - 1;
-  // const markers = document.querySelectorAll('a-marker');
+  // f12 - New Variables
+  let puzzlePieces = numCol * numRow;
+  let imagePieces = new Array(puzzlePieces);
+  let puzzle = [...imagePieces.keys()].map(String);
+  let markers = document.querySelectorAll('a-marker');
   // f14
-  // const positionMarkers = [];
-  // const check = new Array(6);
+  // let positionMarkers = [];
+  // let check = new Array(6);
 
   // f08
   const init = () => {
@@ -59,14 +58,16 @@
       .catch((error) => console.log('takePhoto() error: ', error))
       .then((img) => {
         image.src = URL.createObjectURL(img);
-        // f11 - New Code
+        // f11
         image.addEventListener('load', () => createImagePieces(img));
+        // console log during testing - remove from final code
+        console.log('puzzle', puzzle);
         // f12 - New Code
-        setInterval(() => checkDistance(), 1000);
+        // setInterval(() => checkDistance(), 1000);
       });
   };
 
-  // f11 - New Code
+  // f11
   const createImagePieces = (image) => {
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
@@ -76,10 +77,23 @@
     for (let x = 0; x < numCol; x++) {
       for (let y = 0; y < numRow; y++) {
         ctx.drawImage(image, x * pieceWidth, y * pieceHeight, pieceWidth, pieceHeight, 0, 0, canvas.width, canvas.height);
+        // f12 - new code
+        imagePieces[x * numCol + y] = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+        // console log during testing - remove from final code
+        console.log(imagePieces);
       }
-      
     }
 
+    markers.forEach((marker, index) => {
+      const aImg = new document.createElement('a-image');
+
+      aImg.setAttribute('rotation', '-90, 0, 0');
+      aImg.setAttribute('position', '0, 0, 0');
+      aImg.setAttribute('src', imagePieces[puzzle[index]]);
+      console.log('marker (puzzle): ', index, puzzle[index], imagePieces[puzzle[index]]);
+      console.log('marker (without puzzle): ', index, puzzle[index], imagePieces[index]);
+      marker.appendChild(aImg);
+    });
   }
 
   // f08
